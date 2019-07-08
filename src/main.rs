a491602b212c886a13a6ca569d0ace2914c78f7b
Rainux Luo <rainux@gmail.com>
2019-07-08 13:18:44 +0800

    A minimal hello world with libc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+// Pull in the system libc library for what crt0.o likely requires.
+extern crate libc;
+
+use libc::*;
+
+// Entry point for this program.
+#[no_mangle] // ensure that this symbol is called `main` in the output
+pub extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+    // Since we are passing a C string the final null character is mandatory
+    let hello = "Hello, minimal Rust!\n\0";
+    unsafe {
+        printf(hello.as_ptr() as *const _);
+    }
+    0
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
